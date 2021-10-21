a9c28ae93a890ed853260aadb27b5142c3933302
Brahiam Tabares <dbtabaresv@uqvirtual.edu.co>
2021-10-20 23:15:44 -0500

    se crea la clase SubastaUsuarioTest funcionando completamente

--- persistencia/src/test/resources/subastaUsuario.sql
+++ persistencia/src/test/resources/subastaUsuario.sql
@@ -1,3 +1,3 @@
-insert into SubastaUsuario values ("1","2021-10-10",150000,"10","0006");
-insert into SubastaUsuario values ("2","2021-09-02",50000,"20","0003");
-insert into SubastaUsuario values ("3","2021-10-04",150000,"30","0006");
+insert into Subasta_Usuario values ("1","2021-10-10",150000,"10","00006");
+insert into Subasta_Usuario values ("2","2021-09-02",50000,"20","00003");
+insert into Subasta_Usuario values ("3","2021-10-04",150000,"30","00006");
