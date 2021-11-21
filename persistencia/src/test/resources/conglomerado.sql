4bbb94ba0afc8bef044f3516fcbc8efa7113a3f6
Sandra Milena Quintero Leal <smquinterol@uqvirtual.edu.co>
2021-11-21 11:13:27 -0500

    se anexa en los archivos.sql el username

--- persistencia/src/test/resources/conglomerado.sql
+++ persistencia/src/test/resources/conglomerado.sql
@@ -10,12 +10,12 @@ insert into Ciudad values ("5","Medellín");
 
 -- Persona
 
-insert into Persona values ("00001","administrador@unishop.com", "Sandra Quintero", "123shop");
-insert into Persona values ("00002","camilo_luz@gmail.com", "Camilo Ortega", "123shop");
-insert into Persona values ("00003","luisaMaria@hotmail.com", "Luisa Castro", "456shop");
-insert into Persona values ("00004","administrador2@unishop.com", "Alvaro Arias", "123shop");
-insert into Persona values ("00005","administrador3@unishop.com", "Brahiam Tabares", "123shop");
-insert into Persona values ("00006","carito_quindio@gmail.com", "Carolina Jimenez", "123456");
+insert into Persona values ("00001","administrador@unishop.com", "Sandra Quintero", "123shop","sandris");
+insert into Persona values ("00002","camilo_luz@gmail.com", "Camilo Ortega", "123shop", "camilin");
+insert into Persona values ("00003","luisaMaria@hotmail.com", "Luisa Castro", "456shop","luisita");
+insert into Persona values ("00004","administrador2@unishop.com", "Alvaro Arias", "123shop", "alvarin");
+insert into Persona values ("00005","administrador3@unishop.com", "Brahiam Tabares", "123shop", "brahims");
+insert into Persona values ("00006","carito_quindio@gmail.com", "Carolina Jimenez", "123456","carolis");
 
 -- Administrador
 
