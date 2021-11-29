2f538207f2d94a503d17aaaeaa71c22328f28a05
Brahiam Tabares <dbtabaresv@uqvirtual.edu.co>
2021-11-29 01:44:00 -0500

    se termina el envioTest

--- persistencia/src/test/java/co/edu/uniquindio/proyecto/test/EnvioTest.java
+++ persistencia/src/test/java/co/edu/uniquindio/proyecto/test/EnvioTest.java
@@ -1,10 +1,13 @@
 package co.edu.uniquindio.proyecto.test;
 
+import co.edu.uniquindio.proyecto.entidades.Comentario;
 import co.edu.uniquindio.proyecto.entidades.Compra;
 import co.edu.uniquindio.proyecto.entidades.Envio;
 import co.edu.uniquindio.proyecto.repositorios.CompraRepo;
 import co.edu.uniquindio.proyecto.repositorios.EnvioRepo;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIf;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -13,6 +16,7 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.validation.beanvalidation.SpringValidatorAdapter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -32,13 +36,38 @@ public class EnvioTest {
 
           Envio envio = new Envio("1235", LocalDateTime.now(),"Envia","Bogota carr15 local 3",
                   "Armenia Barrio La fachada mz 21 #2",compra);
+     }
+     @Test // Prueba para Eliminar un Envio
+     @Sql({"classpath:conglomerado.sql"})
+     public void eliminarTest() {
 
+          envioRepo.deleteById("12");
+          Envio envioRegistrado= envioRepo.findById("12").orElse(null);
+          Assertions.assertNull(envioRegistrado);
+     }
 
+     @Test // Prueba de actualización de un Envio
+     @Sql({"classpath:conglomerado.sql"})
+     public void actualizarTest() {
+          Envio envioRegistrado = envioRepo.findById("54").orElse(null);
+          envioRegistrado.setNombreEmpraTransporta("cordinadora");
+          envioRepo.save(envioRegistrado);
+          Envio envioBuscado =envioRepo.findById("54").orElse(null);
+          Assertions.assertEquals("cordinadora", envioBuscado.getNombreEmpraTransporta());
 
+     }
+
+     @Test // Se listan los envios
+     @Sql({"classpath:conglomerado.sql"})
+     public void ListarTest() {
+          List<Envio> envios = envioRepo.findAll();
+          envios.forEach(Envio -> System.out.println(Envio));
      }
 
 
 
 
 
-}
+
+
+     }
